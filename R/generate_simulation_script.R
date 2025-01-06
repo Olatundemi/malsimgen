6fa0fb8523e4244ff26689e6f1555e7533d6258b
Olatunde Ibrahim <ibrahimolatunde4j@gmail.com>
2025-01-06 12:57:01 +0000

    simulation script with hypercow initiated

--- R/generate_simulation_script.R
+++ R/generate_simulation_script.R
@@ -1,12 +1,13 @@
-library(hipercow)
-windows_authenticate()
+# library(hipercow)
+# windows_authenticate()
+#
+# # Initialize hipercow
+#
+# hipercow_init(driver = 'windows')
+# resources <- hipercow_resources(cores=32)
+# hipercow_environment_create(sources = "generate_sim_compendium.R")
 
-# Initialize hipercow
-
-hipercow_init(driver = 'windows')
-resources <- hipercow_resources(cores=32)
-hipercow_environment_create(sources = "generate_sim_compendium.R")
-
+source("R/generate_sim_compendium.R")
 set.seed(123)
 n <- 10  # Number of samples
 log_min <- log10(0.1)
@@ -18,10 +19,10 @@ random_samples <- 10^runif(n, min = log_min, max = log_max)
 volatility_samples <- seq(0.1, 1.0, by = 0.1)
 
 # Number of simulations
-n_sims <- 10000
+n_sims <- 10#000
 
 #task for the simulations
-sims_compendium_train <- task_create_expr({
+sims_compendium_train <- #task_create_expr({
   sims <- lapply(1:n_sims, function(i) {
 
     # Sample init_EIR and volatility for each simulation run
@@ -45,11 +46,9 @@ sims_compendium_train <- task_create_expr({
   combined_sims$run <- rep(1:n_sims, each = nrow(combined_sims) / n_sims)
 
   return(combined_sims)
-}, resources = resources)
-task_status(sims_compendium_train)
-
-#task_log_show(sims_compendium_train)
-file = task_result(sims_compendium_train)
-write.csv(file, "./ANC_Simulation_10000_runs.csv")
-
-use_github(private = FALSE)
+# }, resources = resources)
+# task_status(sims_compendium_train)
+#
+# #task_log_show(sims_compendium_train)
+# file = task_result(sims_compendium_train)
+# write.csv(file, "./ANC_Simulation_10000_runs.csv")
